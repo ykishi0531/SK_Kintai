9eb254f42644283b4826a2b1970399cea70d9738
ryohirose0103 <ryohirose0103@gmail.co.jp>
2017-06-24 17:29:28 +0900

    0624進行分

--- app/src/main/java/jp/co/sakusaku/sk_kintai/activity/StampingActivity.java
+++ app/src/main/java/jp/co/sakusaku/sk_kintai/activity/StampingActivity.java
@@ -2,9 +2,13 @@ package jp.co.sakusaku.sk_kintai.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.Date;
+
+import jp.co.sakusaku.sk_kintai.R;
 
 /**
  * Created by rhirose on 2017/04/15.
@@ -35,9 +39,45 @@ public class StampingActivity extends AppCompatActivity {
         long millsec = System.currentTimeMillis();
         final Date now = getSysDate(millsec);
 
+        String[] week_name = {"日","月","火","水","木","金","土"};
+
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int date = calendar.get(Calendar.DATE);
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        int week = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+        // 現在日付
+        StringBuilder nowDate = new StringBuilder();
+        nowDate = nowDate.append(year);
+        nowDate = nowDate.append("年");
+        nowDate = nowDate.append(month);
+        nowDate = nowDate.append("月");
+        nowDate = nowDate.append(date);
+        nowDate = nowDate.append("日");
+        nowDate = nowDate.append("(");
+        nowDate = nowDate.append(week_name[week]);
+        nowDate = nowDate.append(")");
+
         // 画面へセット
-        TextView nowDate = (TextView) findViewById(R.id.now);
-        nowDate.setText(now.toString());
+        TextView nowDt = (TextView) findViewById(R.id.nowDate);
+        nowDt.setText(nowDate);
+
+        // 現在時刻
+        StringBuilder nowTime = new StringBuilder();
+        nowTime = nowTime.append(hour);
+        nowTime = nowTime.append(" : ");
+        nowTime = nowTime.append(month);
+        nowTime = nowTime.append(" : ");
+        nowTime = nowTime.append(second);
+
+        // 画面へセット
+        TextView nowTm = (TextView) findViewById(R.id.nowTime);
+        nowTm.setText(nowTime);
 
         // 出勤時間を表示
         final String at = "出勤時間　:　" ;
@@ -47,15 +87,16 @@ public class StampingActivity extends AppCompatActivity {
 
 
         // 退勤時間を表示
-        String lv = "退勤時間　:　";
+        final String lv = "退勤時間　:　";
 
         TextView leave_time = (TextView) findViewById(R.id.leave);
         leave_time.setText(lv);
 
+        Button atbtn = (Button) findViewById(R.id.attendbtn);
 
         // 出勤ボタン押下時のイベントを設定する
         // TODO 出退勤テーブルに値を登録する・出勤時間に値をセットする
-        findViewById(R.id.attendance).setOnClickListener(new View.OnClickListener() {
+        atbtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -72,9 +113,11 @@ public class StampingActivity extends AppCompatActivity {
             }
         });
 
+        Button lvbtn = (Button) findViewById(R.id.leavebtn);
+
         // 退勤ボタン押下時のイベントを設定する
 
-        findViewById(R.id.leaving).setOnClickListener(new View.OnClickListener() {
+        lvbtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
